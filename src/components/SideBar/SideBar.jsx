21bde90a9b613f75e9c2a921fe91a05b8a845920
Toaduhs-dev <roguemuffinn@gmail.com>
2026-02-11 19:10:51 -0800

    Added Name

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,6 +1,10 @@
 import "./SideBar.css";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import avatar from "../../images/avatarheader.svg";
 function SideBar({ onProfileChange }) {
+  const currentUser = useContext(CurrentUserContext);
+  const name = currentUser?.name || "";
+  const avatar = currentUser?.avatar || "";
   return (
     <div className="sidebar">
       <div className="sidebar__row">
@@ -9,7 +13,7 @@ function SideBar({ onProfileChange }) {
           src={avatar}
           alt="Daniel Kings Avatar"
         />
-        <p className="sidebar__username">Daniel King</p>
+        <p className="sidebar__username">{name}</p>
       </div>
       <ul className="sidebar__nav">
         <li className="sidebar__nav-item">
